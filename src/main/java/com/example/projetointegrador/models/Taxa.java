793c3d2f11215384fffdd46f8877a38f2d253190
KAYSILVHA <114036212+KAYSILVHA@users.noreply.github.com>
2022-10-26 22:17:15 -0300

    Update Taxa.java

--- src/main/java/com/example/projetointegrador/models/Taxa.java
+++ src/main/java/com/example/projetointegrador/models/Taxa.java
@@ -5,11 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name="taxa")
 public class Taxa {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//identificador automatico
+
+    @Column(name="id")
+    private long id;
+    
+    @Column(name="nome")
     private String nome;
+    
+    @Column(name="porcentagem")
     private String porcentagem;
 }
