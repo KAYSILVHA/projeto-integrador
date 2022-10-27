2725858909bb4d5efdce69557ec98ec357f00ae0
KAYSILVHA <114036212+KAYSILVHA@users.noreply.github.com>
2022-10-26 22:14:29 -0300

    Update Endereco.java

--- src/main/java/com/example/projetointegrador/models/Endereco.java
+++ src/main/java/com/example/projetointegrador/models/Endereco.java
@@ -5,12 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name="endereco")
+
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//identificador automatico
+    
+    @Column(name="id")
+    private long id;
+    
+    @Column(name="logradouro")
     private String logradouro;
+    
+    @Column(name="numeroCasa")
     private String numeroCasa;
+    
+    @Column(name="referencia")
     private String referencia;
 }
