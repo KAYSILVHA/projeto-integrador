663b3267d932a3893fd51cf4f4d487c18e9bcc95
KAYSILVHA <114036212+KAYSILVHA@users.noreply.github.com>
2022-11-04 20:19:49 -0300

    Update DocumentoDTO.java

--- src/main/java/com/example/projetointegrador/dto/DocumentoDTO.java
+++ src/main/java/com/example/projetointegrador/dto/DocumentoDTO.java
@@ -5,7 +5,7 @@ import java.util.Date;
 @Data
 
 public class DocumentoDTO {
-    private Long id;
+    private Long id_documento;
     private String cpf;
     private String identidade;
     private String cnpj;
