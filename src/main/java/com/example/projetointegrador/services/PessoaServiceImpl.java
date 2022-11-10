c55379e051e5a0a09c1d642e418a493c7895df45
KAYSILVHA <kaysilvha@gmail.com>
2022-11-10 19:49:18 -0300

    ProjetoIntegrador

--- src/main/java/com/example/projetointegrador/services/PessoaServiceImpl.java
+++ src/main/java/com/example/projetointegrador/services/PessoaServiceImpl.java
@@ -1,8 +1,5 @@
 package com.example.projetointegrador.services;
 
-import com.example.projetointegrador.dto.CarteiraDTO;
-import com.example.projetointegrador.dto.PessoaDTO;
-import com.example.projetointegrador.models.Carteira;
 import com.example.projetointegrador.models.Pessoa;
 import com.example.projetointegrador.repositories.PessoaRepository;
 import org.springframework.stereotype.Service;
@@ -10,44 +7,46 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PessoaServiceImpl implements PessoaService{
+public class PessoaServiceImpl implements PessoaService {
     final PessoaRepository pessoaRepository;
 
     public PessoaServiceImpl(PessoaRepository pessoaRepository) {
         this.pessoaRepository = pessoaRepository;
     }
-   @Override
-    public List<Pessoa> listar(){
-        return pessoaRepository.findAll();
-   }
-   @Override
-    public Pessoa editar(Pessoa pessoa){
-        return pessoaRepository.save(pessoa);
-   }
-   @Override
-   public Pessoa salvar(Pessoa pessoa) throws Exception {
-
-
-       List<Pessoa> listaDePessoa = pessoaRepository.findAll();
-
-       for(Pessoa pessoa1 : listaDePessoa){
-           if(pessoa.getDocumento().getCpf().equals(pessoa1.getDocumento().getCpf()))
-
-       { throw new Exception("Cpf ja cadastrado, por favor digite outro!");
-
-       }else if(pessoa.getDocumento().getIdentidade().equals(pessoa1.getDocumento().getIdentidade())){
-               throw new Exception("Identidade ja cadastrada, por favor digite outro!");
-           }else if (pessoa.getEndereco().getNumeroCasa().equals(pessoa1.getEndereco().getNumeroCasa()))
-
-               { throw new Exception("Numero de casa ja cadastrado, por favor digite outro!");
-
-               }
-       }return pessoaRepository.save(pessoa);
-
-   }
 
     @Override
-        public void deletar(Long id_pessoa){
+    public List<Pessoa> listar() {
+        return pessoaRepository.findAll();
+    }
+
+    @Override
+    public Pessoa editar(Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+
+    @Override
+    public Pessoa salvar(Pessoa pessoa) throws Exception {
+
+
+        List<Pessoa> listaDePessoa = pessoaRepository.findAll();
+
+        for (Pessoa pessoa1 : listaDePessoa) {
+            if (pessoa.getDocumento().getCpf().equals(pessoa1.getDocumento().getCpf())) {
+                throw new Exception("Cpf ja cadastrado, por favor digite outro!");
+
+            } else if (pessoa.getDocumento().getIdentidade().equals(pessoa1.getDocumento().getIdentidade())) {
+                throw new Exception("Identidade ja cadastrada, por favor digite outro!");
+            } else if (pessoa.getEndereco().getNumeroCasa().equals(pessoa1.getEndereco().getNumeroCasa())) {
+                throw new Exception("Numero de casa ja cadastrado, por favor digite outro!");
+
+            }
+        }
+        return pessoaRepository.save(pessoa);
+
+    }
+
+    @Override
+    public void deletar(Long id_pessoa) {
         pessoaRepository.deleteById(id_pessoa);
     }
 }
