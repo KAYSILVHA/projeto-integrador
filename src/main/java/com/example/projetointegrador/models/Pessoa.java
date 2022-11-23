da41001b2f825be931854e7cc373696f124fe4dc
KAYSILVHA <kaysilvha@gmail.com>
2022-11-22 22:25:35 -0300

    Projeto_Poupa+

--- src/main/java/com/example/projetointegrador/models/Pessoa.java
+++ src/main/java/com/example/projetointegrador/models/Pessoa.java
@@ -1,9 +1,6 @@
 package com.example.projetointegrador.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -14,6 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode(of = "id")
 @Table(name="pessoa")
 public class Pessoa {
     @Id
---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.net.URL;
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo(){
+
+        return new ApiInfo("Projeto_Integrador Rest APIs", "API POUPA+", "1.0", "Termos de Serviço",
+                    new Contact("Kayra","www.org.com", "kayrasilvha@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+
+    }}
